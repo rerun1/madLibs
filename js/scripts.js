9f7f81d171cee5a35bda7127d3451f6b423d1c99
Suzi Rubino <suzi@suzirubi.com>
2019-08-09 13:27:37 -0700

    Refactor and add new font

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
+var resetForm = function(input){
+    $("input").val("");
+}
+
 $(document).ready (function(){
 
-  $("#formOne").submit(function(event){
+  $("form#formOne").submit(function(event){
+    var userInput = "";
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    var person1Input = $("input#person1").val();;
-    var person2Input = $("input#person2").val();;
-    var animalInput = $("input#animal").val();;
-    var exclamationInput = $("input#exclamation").val();;
-    var verbInput = $("input#verb").val();;
-    var nounInput = $("input#noun").val();;
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("."+ blank).text(userInput);
+    });
 
     $("#story").show();
+    $("form#formOne").hide();
+    $("h1#intro").hide();
+
+    resetForm($("form#formOne"));
 
     event.preventDefault();
 
   });
+
+  $("button#makeAnother").click(function(){
+    $("#story").hide();
+    $("form#formOne").show();
+    $("h1#intro").show();
+  });
+
 
 });
